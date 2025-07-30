64dd776adfabddf5727589ac79d3d5aa61a85d15
leyinlin <hello@leyinlin>
2025-07-30 18:07:42 +0800

    Initial commit

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,70 @@
+const base = "/blog-demo/"; 
+export default {
+   head: [
+    ["link", { rel: "icon", href: "/boke.ico" }],
+  ],
+  title: '是乐茵lin的博客',
+  description: '是乐茵lin用来写博客的地方',
+   themeConfig: {
+    logo: '/bcImgs.png',
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '关于', link: '/about' },
+      {
+        text: 'golang后端',
+        items: [
+          { text: "go基础", link: "/golang/go基础/" },
+          { text: "go进阶", link: "/golang/go进阶/" },
+          { text: "go底层知识", link: "/golang/go底层知识/" },
+        ]
+      }
+    ],
+   sidebar: {
+      '/golang/go基础/': [
+        { text: '介绍', link: '/golang/go基础/' },
+        { text: 'go基础1', link: '/golang/go基础/go基础1' },
+        { text: 'go基础2', link: '/golang/go基础/go基础2' },
+      ],
+      '/golang/go进阶/': [
+        { text: '介绍', link: '/golang/go进阶/' },
+        { text: 'go进阶1', link: '/golang/go进阶/go进阶1' },
+        { text: 'go进阶2', link: '/golang/go进阶/go进阶2' },
+      ],
+      '/golang/go底层知识/': [
+        { text: '介绍', link: '/golang/go底层知识/' },
+        { text: 'go底层知识1', link: '/golang/go底层知识/go底层知识1' },
+        { text: 'go底层知识2', link: '/golang/go底层知识/go底层知识2' },
+      ]
+    },
+    docFooter: {
+      prev: '上一篇',
+      next: '下一篇',
+    },
+   footer: {
+      message: '版权所有 © 2024 是乐茵lin的博客',
+      copyright: 'Released under the MIT License'
+    },
+   hero: {
+      name: '是乐茵lin的博客',
+      text: 'awesome-front-end-world.',
+      tagline: '前端 知识体系地图',
+      image: {
+        src: '/bclmgs.png', 
+        alt: 'Logo'
+      },
+      actions: [
+        {
+          theme: 'brand',
+          text: 'Get Started',
+          link: '/golang/go基础/'
+        },
+       {
+          theme: 'alt',
+          text: 'View on GitHub',
+          link: 'https://github.com/vuejs/vitepress'
+        }
+      ]
+    }
+  }
+}
+
---
+++ docs/index.md
@@ -0,0 +1,40 @@
+---
+layout: home
+
+hero:
+  name: 是乐茵lin的博客
+  text: Welcome to the world of golang.
+  tagline: 后端Golang 知识体系地图
+  image:
+    src: /boke.png
+    alt: logo
+  actions:
+    - theme: brand
+      text: Get Started
+      link: /golang/go基础
+    - theme: alt
+      text: View on GitHub
+      link: https://github.com/vuejs/vitepress
+---
+
+<style>
+:root {
+  --vp-home-hero-name-color: transparent;
+  --vp-home-hero-name-background: -webkit-linear-gradient(120deg, #bd34fe 30%, #41d1ff);
+
+  --vp-home-hero-image-background-image: linear-gradient(-45deg, #bd34fe 50%, #47caff 50%);
+  --vp-home-hero-image-filter: blur(44px);
+}
+
+@media (min-width: 640px) {
+  :root {
+    --vp-home-hero-image-filter: blur(56px);
+  }
+}
+
+@media (min-width: 960px) {
+  :root {
+    --vp-home-hero-image-filter: blur(68px);
+  }
+}
+</style>
